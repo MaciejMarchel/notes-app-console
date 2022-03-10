39f4deae8686c594d8e388a7e20b1d9ea00732d6
Maciej <maciejmarchel14@gmail.com>
2022-03-10 12:36:01 +0000

    Added extra functionality to testing

--- src/main/kotlin/models/Note.kt
+++ src/main/kotlin/models/Note.kt
@@ -3,5 +3,6 @@ package models
 data class Note(val noteTitle: String,
                 val notePriority: Int,
                 val noteCategory: String,
-                val isNoteArchieved: Boolean) {
+                val isNoteArchieved: Boolean,
+                val isNoteArchived: Boolean){
 }
